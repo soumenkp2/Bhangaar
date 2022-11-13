acc021c3dd890b5b2d7a5fa22f6ad5e205903ec5
soumenkp2 <soumenkp2@gmail.com>
2022-11-13 15:30:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bhangaar/OnBoardingScreen.kt
@@ -0,0 +1,32 @@
+package com.example.bhangaar
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class OnBoardingScreen : AppCompatActivity() {
+
+    private lateinit var register_btn : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding_screen)
+
+        init();
+
+        register_btn.setOnClickListener(
+            View.OnClickListener {
+                val intent = Intent(this@OnBoardingScreen, SignUp :: class.java);
+                startActivity(intent);
+            }
+        )
+
+
+    }
+
+    private fun init(){
+        register_btn = findViewById(R.id.signup_btn);
+    }
+}
